4a65fde8f301ab60ff80544ced85a6b2e7c642ba
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-08-21 17:22:08 +0900

    [accepted] ALDS1_2_C in C++

--- ALDS/ALDS1_2_C.cpp
+++ ALDS/ALDS1_2_C.cpp
@@ -27,6 +27,7 @@ bool isStable(Card in[], Card out[], int n) {
           if (in[i].value == in[i].value && in[i].suit == out[b].suit &&
               in[i].value == out[b].value && in[j].suit == out[a].suit &&
               in[j].value == out[a].value)
+            // 安定でない場合
             return false;
         }
       }
@@ -35,6 +36,37 @@ bool isStable(Card in[], Card out[], int n) {
 
   // 安定な場合
   return true;
+}
+
+void bubbleSort(Card c[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = n - 1; j > i; j--) {
+      if (c[j].value < c[j - 1].value) {
+        // swap
+        Card tmp;
+        tmp = c[j];
+        c[j] = c[j - 1];
+        c[j - 1] = tmp;
+      }
+    }
+  }
+}
+
+void selectionSort(Card c[], int n) {
+  for (int i = 0; i < n; i++) {
+    int minj = i;
+    for (int j = i; j < n; j++) {
+      if (c[j].value < c[minj].value) {
+        minj = j;
+      }
+    }
+
+    // swap
+    Card tmp;
+    tmp = c[i];
+    c[i] = c[minj];
+    c[minj] = tmp;
+  }
 }
 
 int main() {
@@ -52,11 +84,16 @@ int main() {
     c2[i] = c1[i];
   }
 
+  bubbleSort(c1, n);
   print(c1, n);
+  cout << "Stable" << endl;
+
+  selectionSort(c2, n);
+  print(c2, n);
   if (isStable(c1, c2, n)) {
-    cout << "stable" << endl;
+    cout << "Stable" << endl;
   } else {
-    cout << "unstable" << endl;
+    cout << "Not stable" << endl;
   }
 
   return 0;
