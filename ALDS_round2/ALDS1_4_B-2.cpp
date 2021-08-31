c0ebdce8f37ab2591ce6560e5361cca1efd46caa
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-08-31 14:00:08 +0900

    [accepted] ALDS1_4_B: retry

---
+++ ALDS_round2/ALDS1_4_B-2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX_N = 100000;
+const int MAX_Q = 50000;
+
+int S[MAX_N];
+int T[MAX_Q];
+
+bool binarySearch(int A[], int n, int key) {
+  int mid;
+  int left = 0;
+  int right = n;
+
+  while (left < right) {
+    mid = (left + right) / 2;
+    if (A[mid] == key) {
+      return true;
+    } else if (A[mid] > key) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return false;
+}
+
+int main() {
+  int n, q;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> S[i];
+  }
+
+  cin >> q;
+
+  for (int i = 0; i < q; i++) {
+    int key;
+    cin >> T[i];
+  }
+
+  int cnt;
+  for (int i = 0; i < q; i++) {
+    if (binarySearch(S, n, T[i])) {
+      cnt++;
+    }
+  }
+
+  cout << cnt << endl;
+
+  return 0;
+}
